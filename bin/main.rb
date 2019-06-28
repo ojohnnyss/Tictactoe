53d1c38489ec195780003983805e7af2306422be
Juri <ojohnnyss@yahoo.com>
2019-06-28 18:30:05 +0300

    Update main.rb

--- bin/main.rb
+++ bin/main.rb
@@ -16,9 +16,9 @@ def start
     puts "Current board:"
     game.render_board
     if result == 1
-    puts "#{$name1} wins!"
+      puts "#{$name1} wins!"
     else
-    puts "#{$name2} wins!"
+      puts "#{$name2} wins!"
     end
     if result == 1 || result == 2
       puts "It was exciting! Bye-bye."
