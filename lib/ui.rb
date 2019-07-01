6fa5038e308dbd3d02e4633116b0e9d2300679c4
Juri <ojohnnyss@yahoo.com>
2019-07-01 13:24:48 +0300

    Update ui.rb

--- lib/ui.rb
+++ lib/ui.rb
@@ -27,5 +27,16 @@ def iput_player
    end
   end
 end  
+
+def print_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "___________"
+  puts
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "___________"
+  puts
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+  puts
+end
   
 end
