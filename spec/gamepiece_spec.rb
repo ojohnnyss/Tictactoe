ad5b443b60e327ee8339612ee5ca94291831a155
Juri <ojohnnyss@yahoo.com>
2019-07-02 16:24:10 +0300

    Update gamepiece_spec.rb

--- spec/gamepiece_spec.rb
+++ spec/gamepiece_spec.rb
@@ -1,12 +1,21 @@
 require '../lib/gamepiece'
 
-#Gamepiece
-RSpec.describe Gamepiece do
+#GamePiece
+RSpec.describe GamePiece do
   before do
-    @type = GamePiece.new()
-    @pos = GamePiece.new()
+    @type = GamePiece.new(1, 4)
+    @type1 = GamePiece.new(2, 9)
+    @type2 = GamePiece.new(3,12)
   end
 
-  
-
+  context '# a player gets a position' do
+    it 'a player gets new position on board' do
+      expect(@type.type).to eql(1)
+      expect(@type1.type).to eql(2)
+      expect(@type.pos).to eql(4)
+      expect(@type1.pos).to eql(9)
+      expect(@type2.type).not_to be_nil
+      expect(@type2.pos).not_to be_nil
+    end
+  end
 end
